26722eddad2af5be0a7c56755b03d4a80477bb2e
Jesper Olsson <c.jesper.olsson@gmail.com>
2023-02-26 13:43:46 +0100

    Trying to add adaptive svg icons with embedded @media dark theme query.
    But chrome does not want to load svg files/content as extension toolbar
    icons. And one cannot use window.matchMedia() in background script
    anymore so cannot switch between icons there.like in v2 extensions.

--- service_worker.js
+++ service_worker.js
@@ -4,6 +4,7 @@ chrome.action.onClicked.addListener((tab) => {
 
 chrome.tabs.onCreated.addListener((tab) => {
     groupTabsPerchance(false);
+    // chrome.action.setIcon({ path: { 16: "icons/tab_group_FILL0_wght400_GRAD0_opsz48_dark_light_embedded_css.svg" } });
 });
 
 function groupTabsPerchance(toggle) {
@@ -21,13 +22,14 @@ function groupTabsPerchance(toggle) {
                 
                 switch(groups.length) {
                     case 0:
-                        let groupId = await chrome.tabs.group({ tabIds: tabIds });
-                        await chrome.tabGroups.update(groupId, { title: "JO" });
+                        chrome.tabs.group({ tabIds: tabIds }).then((groupId) => {
+                            chrome.tabGroups.update(groupId, { title: "JO", color: "grey" });
+                        });
                         console.log("No tabgroups detected, creating one for all tabs");
                         break;
                     case 1:
                         // tabs.query( tabGroup:tabGroups.TAB_GROUP_ID_NONE 
-                        await chrome.tabs.group({ tabIds: tabIds, groupId: groups[0].id });
+                        chrome.tabs.group({ tabIds: tabIds, groupId: groups[0].id });
                         console.log("1 group found, collectinng all tabs in that one...")
                         break;
                     default:
